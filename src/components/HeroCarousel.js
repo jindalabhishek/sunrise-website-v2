65095ae8be1393af26035785c9490e4b62bbfaaf
abhishek jindal <abhishekjindal21@gmail.com>
2025-02-21 03:20:21 -0800

    correct sizes of carousel, form and phone numbers

--- src/components/HeroCarousel.js
+++ src/components/HeroCarousel.js
@@ -9,12 +9,12 @@ const HeroCarousel = () => {
     {/* Centered h3 below the carousel */}
     <h3 className="text-xl md:text-2xl font-semibold mb-6 text-center text-red-700">Empowering Education for a Brighter Future</h3>
   <br/>
-  <Carousel autoPlay={true} infiniteLoop showThumbs={false} className="w-[80%] md:w-[60%] shadow-xl">
-    <img src="/images/cover4.jpg" className="w-3/5 h-[200px] md:h-[400px]" />
-    <img src="/images/cover1.jpeg" className="w-3/5 h-[200px] md:h-[400px]" />
-    <img src="/images/cover3.jpeg" className="w-3/5 h-[200px] md:h-[400px]" />
-    <img src="/images/cover5.jpeg" className="w-3/5 h-[200px] md:h-[400px]" />
-    <img src="/images/cover6.jpeg" className="w-3/5 h-[200px] md:h-[400px]" />
+  <Carousel autoPlay={true} infiniteLoop showThumbs={false} className="w-[full] md:w-[70%] shadow-xl">
+    <img src="/images/cover4.jpg" className="w-[90%] h-[180px] md:h-[400px]" />
+    <img src="/images/cover1.jpeg" className="w-[90%] h-[180px] md:h-[400px]" />
+    <img src="/images/cover3.jpeg" className="w-[90%] h-[180px] md:h-[400px]" />
+    <img src="/images/cover5.jpeg" className="w-[90%] h-[180px] md:h-[400px]" />
+    <img src="/images/cover6.jpeg" className="w-[90%] h-[180px] md:h-[400px]" />
   </Carousel>
   
 </div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,11 +10,11 @@ const Contact = () => {
             <div className="flex-1 mb-6 md:mr-4">
                 <h2 className="text-xl font-bold mb-4 text-red-700">Contact Us</h2>
                 <p className="mb-2"><FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" /><strong className="text-red-700">Address:</strong> <span className="text-blue-500">KOT ROAD, PANDUSAR MOHALLA, NABHA, 147201</span></p>
-                <p className="mb-2"><FontAwesomeIcon icon={faPhone} className="mr-2" /><strong className="text-red-700">Phone:</strong> <span className="text-blue-500">(123) 456-7890</span></p>
+                <p className="mb-2"><FontAwesomeIcon icon={faPhone} className="mr-2" /><strong className="text-red-700">Phone:</strong> <span className="text-blue-500">+91-9056744006, +91-9988332831</span></p>
                 <p className="mb-2"><FontAwesomeIcon icon={faEnvelope} className="mr-2" /><strong className="text-red-700">Email:</strong> <span className="text-blue-500">info@sunriseinstitute.com</span></p>
                 <p className="mb-2"><FontAwesomeIcon icon={faClock} className="mr-2" /><strong className="text-red-700">Working Hours:</strong></p>
                 <ul className="list ml-5 mb-4">
-                    <li><FontAwesomeIcon icon={faClock} className="mr-2" /><span className="text-red-700">Monday - Saturday:</span><span className="text-blue-500"> 9:00 AM - 5:00 PM</span></li>
+                    <li><FontAwesomeIcon icon={faClock} className="mr-2" /><span className="text-red-700">Monday - Saturday:</span><span className="text-blue-500"> 2:00 PM - 7:30 PM</span></li>
                     <li><FontAwesomeIcon icon={faClock} className="mr-2" /><span className="text-red-700">Sunday: </span><span className="text-blue-500">Closed</span></li>
                 </ul>
             </div>
@@ -22,7 +22,7 @@ const Contact = () => {
                 <h2 className="text-xl font-bold mb-4 text-red-700">Contact Form</h2>
                 <iframe
                     src="https://docs.google.com/forms/d/e/1FAIpQLSfraWGF6ZvytPNmtAA-_iPO6uJcOyspT4oTZwyBKF1kVo9JLg/viewform?embedded=true" // Replace with your Google Form URL
-                    width="600"
+                    width="100%"
                     height="1000"
                     frameBorder="0"
                     marginHeight="0"
